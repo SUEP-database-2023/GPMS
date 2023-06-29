40968355b75852e75044a557fa281889133c4cbb
zcxxxxxxx <2049314799@qq.com>
2023-06-29 20:58:47 +0800

    修改数据库中，topic_id,student_id,teacher_id等数据类型

--- backend/db/alembic/versions/6_c10b20182c61_create_selection_table.py
+++ backend/db/alembic/versions/6_c10b20182c61_create_selection_table.py
@@ -10,8 +10,8 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'c10b20182c61'
-down_revision = '1eb632ce6098'
+revision = "c10b20182c61"
+down_revision = "1eb632ce6098"
 branch_labels = None
 depends_on = None
 
@@ -26,16 +26,13 @@ def upgrade() -> None:
             index=True,
             comment="序列号",
         ),
-        sa.Column(
-            "student_id", sa.Integer, index=True, comment="学生编号"
-        ),
-        sa.Column("topic1_id", sa.Integer, nullable=False, comment="第一志愿id"),
-        sa.Column("topic2_id", sa.Integer, nullable=False, comment="第二志愿id"),
-        sa.Column("topic3_id", sa.Integer, nullable=False, comment="第三志愿id"),
-        sa.Column("topic4_id", sa.Integer, nullable=False, comment="第四志愿id"),
+        sa.Column("student_id", sa.String(10), index=True, comment="学生编号"),
+        sa.Column("topic1_id", sa.String(9), nullable=False, comment="第一志愿id"),
+        sa.Column("topic2_id", sa.String(9), nullable=False, comment="第二志愿id"),
+        sa.Column("topic3_id", sa.String(9), nullable=False, comment="第三志愿id"),
+        sa.Column("topic4_id", sa.String(9), nullable=False, comment="第四志愿id"),
         sa.Column("select_time", sa.DateTime, nullable=False, comment="选题时间"),
         sa.Column("select_status", sa.Integer, nullable=False, comment="第几轮选题"),
-        
     )
 
 
