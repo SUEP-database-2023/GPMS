c3a460f7853e68db03729403e6426be5d5aea30f
zcxxxxxxx <2049314799@qq.com>
2023-07-07 02:07:14 +0800

    增加点击特效

--- frontend/src/components/Layout/Sidebar/index.jsx
+++ frontend/src/components/Layout/Sidebar/index.jsx
@@ -5,17 +5,21 @@ import Menu from "../../Menu";
 const Sidebar = ({ main, items }) => {
   return (
     <div className="flex h-screen w-screen">
-      <div className="bg-gradient-to-b from-blue-600 to-blue-900 w-1/6 flex flex-col items-center ">
+      <div className="bg-gradient-to-b from-blue-600 to-blue-900 w-[20%] flex flex-col items-center ">
         <div className="w-48 h-48 flex items-center justify-center ">
+          {/* TODO:图片大小 */}
           <img
             src={logo}
             className="object-contain w-[50%] h-full "
             alt="Logo"
           />
         </div>
-        <div className="space-y-8">
+        <div className="space-y-8 w-full">
           <div>
-            <h1 className="text-center text-lg text-white">毕业设计管理系统</h1>
+            {/* TODO:字体 */}
+            <h1 className="text-base sm:text-lg md:text-2xl lg:text-3xl text-center text-white">
+              毕业设计选题管理系统
+            </h1>
           </div>
           <Menu items={items} />
         </div>
--- frontend/src/components/Menu/index.jsx
+++ frontend/src/components/Menu/index.jsx
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+
 const Menu = ({ items }) => {
+  const [selectedItem, setSelectedItem] = useState(null);
+
+  const handleItemClick = (index) => {
+    setSelectedItem(index);
+  };
+
   return (
     <div>
-      <ul className="menu rounded-box space-y-4">
-        {items.map((item) => (
-          // TODO:更为好看的导航栏
-          <li>
-            <a className="text-center text-base">{item.title}</a>
+      <ul className="menu sm:menu-sm md:menu-md lg:menu-lg rounded-box ">
+        {items.map((item, index) => (
+          <li
+            key={index}
+            className={`${
+              selectedItem === index ? "bg-white" : ""
+            } pr-0 rounded-l-full transform transition-transform duration-300 ${
+              selectedItem === index
+                ? "sm:translate-x-1 md:translate-x-5 lg:translate-x-10"
+                : ""
+            }`}
+            onClick={() => handleItemClick(index)}
+          >
+            {/* TODO:图标 + 链接 */}
+            <a className="text-base sm:text-lg md:text-xl lg:text-2xl text-center pr-0 my-1">
+              {item.title}
+            </a>
           </li>
         ))}
       </ul>
