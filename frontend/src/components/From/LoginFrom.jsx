ff0545bc8d83c82f9d18312a29fe1233e2d98d1b
zcxxxxxxx <2049314799@qq.com>
2023-07-08 15:25:54 +0800

    增加todo

--- frontend/src/components/From/LoginFrom.jsx
+++ frontend/src/components/From/LoginFrom.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import { UserOutlined, LockOutlined } from "@ant-design/icons";
 const LoginFrom = () => {
   return (
-    <div className="flex flex-col justify-center space-y-4 w-[50%] h-[35%]">
+    <div className="flex flex-col justify-center space-y-4 w-[50%] h-[25%]">
       <div className="w-full text-center">
         <div className="text-blue-600 text-3xl bg-gray-100 py-2">账号登录</div>
       </div>
       <div className="flex flex-col space-y-4">
         <div className="flex flex-row justify-center items-center border-2">
           <UserOutlined className="text-blue-800 mr-4" />
+          {/* TODO:输入框设置有问题 */}
           <input
             type="text"
             placeholder="请输入账号"
--- frontend/src/pages/LoginPage/LoginPAge.jsx
+++ frontend/src/pages/LoginPage/LoginPAge.jsx
@@ -5,11 +5,13 @@ import LoginPicture from "../../assets/LoginPicture.png";
 const LoginPage = () => {
   return (
     <div className="flex w-screen h-screen bg-white">
-      <div className="h-screen w-[45%] flex flex-col items-center justify-center">
+      <div className="h-screen w-[45%] flex flex-col items-center justify-center mt-[-5%]">
+        {/* TODO:整体上移? */}
         <LoginHead />
         <LoginFrom />
       </div>
       <div className="h-screen w-[55%] bg-blue-400">
+        {/* TODO: 图片设置有问题*/}
         <img src={LoginPicture} className="w-full h-full object-cover" />
       </div>
     </div>
