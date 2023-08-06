07422fdb419a6f6afc7b77df25a8f8bb6d7ecee1
huiyuan-li <1472927952@qq.com>
2023-08-06 20:47:14 +0800

    参数设置，密码修改

--- frontend/src/pages/AdminPage/ParameterSettingPage.jsx
+++ frontend/src/pages/AdminPage/ParameterSettingPage.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import { DatePicker, Space } from 'antd';
+const onChange = (date, dateString) => {
+  console.log(date, dateString);
+};
+const { RangePicker } = DatePicker;
 const ParmeterSettingPage = () => {
   return (
     <div className="flex flex-col w-[100%] h-[100%] items-center bg-white-100">
       <div className="flex flex-col w-[80%] h-[90%] items-start bg-blue-100">
         <div className="flex w-[100%] h-[10%] items-center justify-center bg-blue-200">
-          请注意，此处设置的截止时间均为该日期零点零分零秒
+          请注意，此处设置的截止时间均为该日期xx点xx分xx秒
         </div>
         <div className="flex flex-col w-[100%] h-[90%] items-start justify-center bg-blue-100">
           <div className="flex w-[100%] h-[20%] bg-pink-100">
@@ -13,7 +18,7 @@ const ParmeterSettingPage = () => {
               教师提交题目截止时间
               </div>
               <div className="flex w-[100%] h-[50%] items-center justify-center">   {/*时间组件*/}
-              输入时间组件
+              <Space direction="vertical"><DatePicker onChange={onChange} /></Space>
               </div>
             </div>
             <div className="flex flex-col w-[50%] h-[100%] items-start">
@@ -21,7 +26,7 @@ const ParmeterSettingPage = () => {
               管理员审核题目截止时间
               </div>
               <div className="flex w-[100%] h-[50%] items-center justify-center">   {/*时间组件*/}
-              输入时间组件
+              <Space direction="vertical"><DatePicker onChange={onChange} /></Space>
               </div>
             </div>
           </div>
@@ -31,7 +36,8 @@ const ParmeterSettingPage = () => {
                 学生第一次选题开始时间
                 </div>
                 <div className="flex w-[100%] h-[50%] items-center justify-center">   {/*时间组件*/}
-                输入时间组件
+                {/* 输入时间组件 */}
+                <Space direction="vertical" size={12}><RangePicker /></Space>
                 </div>
               </div>
               <div className="flex flex-col w-[33%] h-[100%] items-start">
@@ -47,7 +53,7 @@ const ParmeterSettingPage = () => {
                 管理员第一次匹配截止时间
                 </div>
                 <div className="flex w-[100%] h-[50%] items-center justify-center">   {/*时间组件*/}
-                时间组件
+                <Space direction="vertical"><DatePicker onChange={onChange} /></Space>
                 </div>
               </div>
           </div>
@@ -57,7 +63,8 @@ const ParmeterSettingPage = () => {
                 学生第二次选题开始时间
                 </div>
                 <div className="flex w-[100%] h-[50%] items-center justify-center">   {/*时间组件*/}
-                输入时间组件
+                {/* 输入时间组件 */}
+                <Space direction="vertical" size={12}><RangePicker /></Space>
                 </div>
               </div>
               <div className="flex flex-col w-[33%] h-[100%] items-start">
@@ -73,12 +80,19 @@ const ParmeterSettingPage = () => {
                 管理员第二次匹配截止时间
                 </div>
                 <div className="flex w-[100%] h-[50%] items-center justify-center">   {/*时间组件*/}
-                时间组件
+                <Space direction="vertical"><DatePicker onChange={onChange} /></Space>
                 </div>
               </div>
           </div>
           <div className="flex w-[100%] h-[20%] items-center justify-center bg-pink-400">
-            提交组件
+          <button
+          onClick={() => {
+            handleLogin();
+          }}
+          className="w-[20%] btn bg-blue-500 border-blue-500 hover:bg-blue-400 hover:border-blue-400 text-white rounded-full"
+        >
+          提交
+        </button>
           </div>
         </div>
       </div>
--- frontend/src/pages/AdminPage/PasswordUpdatePage.jsx
+++ frontend/src/pages/AdminPage/PasswordUpdatePage.jsx
@@ -1,5 +1,31 @@
 import React from "react";
+import { EyeTwoTone, EyeInvisibleOutlined } from "@ant-design/icons";
+import { Button, Input, Space } from "antd";
 const PasswordUpdatePage = () => {
-  return <div className="h-screen">我是修改密码</div>;
+  const [passwordVisible, setPasswordVisible] = React.useState(false);
+  return (
+    <div className="flex flex-col h-screen items-center">
+      <div className="flex flex-col items-center space-y-5 w-[75%] bg-white p-5">
+        <div className="flex flex-col w-[75%] items-center space-y-5 text-blue-800">
+          <div className="text-blue-800 text-3xl pb-10">修改密码</div>
+          <div className="flex flex-row ">
+            <div className=" w-[80%]">请输入账号:</div>
+            <Input.Password
+              placeholder="输入账号"
+              visibilityToggle={{
+                visible: passwordVisible,
+                onVisibleChange: setPasswordVisible,
+              }}
+            />
+          </div>
+          
+         
+          <Button className="w-[37%] bg-blue-600 text-white">
+            重置
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default PasswordUpdatePage;
