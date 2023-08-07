c24c947ff853b26b6689c6d294088d346cfec949
huiyuan-li <1472927952@qq.com>
2023-08-07 16:22:02 +0800

    教师端-选题结果，教师端-提交列表

--- frontend/src/pages/TeacherPage/TopicListPage.jsx
+++ frontend/src/pages/TeacherPage/TopicListPage.jsx
@@ -1,5 +1,43 @@
 import React from "react";
+import { Space, Table, Tag } from "antd";
+const columns = [
+  {
+    title: "序号",
+    dataIndex: "id",
+    key: "id",
+    render: (text) => <a>{text}</a>,
+  },
+  {
+    title: "课题名称",
+    dataIndex: "subject name",
+    key: "subject name",
+  },
+  {
+    title: "适用专业",
+    key: "major",
+    dataIndex: "major",
+  },
+  {
+    title: "操作",
+    key: "action",
+    render: (_, record) => (
+      <Space size="middle">
+        <a>Invite {record.name}</a>
+        <a>Delete</a>
+      </Space>
+    ),
+  },
+];
 const TopicListPage = () => {
-  return <div className="h-screen">我是课题列表</div>;
+  return (
+    <div className="flex flex-col h-screen items-center">
+      <div className="flex flex-col w-[90%] items-center justify-center bg-blue-200">
+      请注意！教师提交毕业设计题目的截止时间：xxxx/xx/xx，届时系统将无法提交和更新课题信息！
+      </div>
+      <div className="flex flex-col w-[90%]">
+        <Table columns={columns} />
+      </div>
+    </div>
+  );
 };
 export default TopicListPage;
--- frontend/src/pages/TeacherPage/TopicResultPage.jsx
+++ frontend/src/pages/TeacherPage/TopicResultPage.jsx
@@ -1,5 +1,55 @@
 import React from "react";
+import { AudioOutlined } from '@ant-design/icons';
+import {Input, Space, Table, Tag } from "antd";
+const columns = [
+  {
+    title: "序号",
+    dataIndex: "id",
+    key: "id",
+    // render: (text) => <a>{text}</a>,
+  },
+  {
+    title: "学号",
+    dataIndex: "student number",
+    key: "student number",
+  },
+  {
+    title: "姓名",
+    dataIndex: "student name",
+    key: "student name",
+  },
+  {
+    title: "专业",
+    dataIndex: "major",
+    key: "major",
+  },
+  {
+    title: "班级",
+    dataIndex: "class",
+    key: "class",
+  },
+  {
+    title: "联系电话",
+    dataIndex: "telephone",
+    key: "telephone",
+  },
+  {
+    title: "课题名称",
+    dataIndex: "subject name",
+    key: "subject name",
+  },
+  
+];
 const TopicResultPage = () => {
-  return <div className="h-screen">我是选课结果</div>;
+  return (
+    <div className="flex flex-col h-screen items-center">
+      <div className="flex flex-col w-[90%] items-center justify-center bg-blue-200">
+      请注意！教师提交毕业设计题目的截止时间：xxxx/xx/xx，届时系统将无法提交和更新课题信息！
+      </div>
+      <div className="flex flex-col w-[90%]">
+        <Table columns={columns} />
+      </div>
+    </div>
+  );
 };
 export default TopicResultPage;
