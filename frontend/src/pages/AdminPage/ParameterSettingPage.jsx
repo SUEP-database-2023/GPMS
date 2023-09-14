38efe802fde2d0679c92b3ae9b534b77c40ecec9
Mright-Red <3112069360@qq.com>
2023-09-15 02:08:21 +0800

    管理员端前端的布局

--- frontend/src/pages/AdminPage/ParameterSettingPage.jsx
+++ frontend/src/pages/AdminPage/ParameterSettingPage.jsx
@@ -46,143 +46,104 @@ const ParmeterSettingPage = () => {
   };
 
   return (
-    <div className="flex flex-col w-[100%] h-[100%] items-center bg-white-100 ">
-      {/* TODO 大改UI及代码结构 */}
-      <div className="flex flex-col w-[80%] h-[90%] items-start bg-blue-100">
-        <div className="flex w-[100%] h-[10%] items-center justify-center bg-blue-200">
+    <div className="flex flex-col w-full h-full items-center ">
+      <div className="flex flex-col w-[80%] h-[80%] items-start bg-white">
+        <div className="flex w-[100%] h-[10%] items-center justify-center bg-blue-500 border border-gray-300">
           请注意，此处设置的截止时间均为该日期00点00分00秒
         </div>
-        <div className="flex flex-col w-[100%] h-[90%] items-start justify-center bg-blue-100">
-          <div className="flex w-[100%] h-[20%] bg-pink-100">
-            <div className="flex flex-col w-[50%] h-[100%] items-start">
-              <div className="flex w-[100%] h-[50%] items-center justify-center">
-                教师提交题目截止时间
-              </div>
-              <div className="flex w-[100%] h-[50%] items-center justify-center">
-                {/*时间组件*/}
-                <Space direction="vertical">
-                  <DatePicker
-                    onChange={(value, dateString) =>
-                      handleManyTime(value, dateString, set_teacher_post_time)
-                    }
-                  />
-                </Space>
-              </div>
-            </div>
-            <div className="flex flex-col w-[50%] h-[100%] items-start">
-              <div className="flex w-[100%] h-[50%] items-center justify-center">
-                管理员审核题目截止时间
-              </div>
-              <div className="flex w-[100%] h-[50%] items-center justify-center">
-                {/*时间组件*/}
-                <Space direction="vertical">
-                  <DatePicker
-                    onChange={(value, dateString) =>
-                      handleManyTime(value, dateString, set_admin_audit_time)
-                    }
-                  />
-                </Space>
-              </div>
-            </div>
+        <div className="flex w-[100%] h-[20%] border border-gray-300">
+          <div className="flex w-[50%] h-[100%] items-center justify-center">
+            ID:
+            <Input onChange={handleID} style={{ width: 100 }} maxLength={1} />
           </div>
-          <div className="flex w-[100%] h-[30%] bg-pink-200">
-            <div className="flex flex-col w-[33%] h-[100%] items-start">
-              <div className="flex w-[100%] h-[50%] items-center justify-center">
-                学生第一次选题开始和截止时间
-              </div>
-              <div className="flex w-[100%] h-[50%] items-center justify-center">
-                {/*时间组件*/}
-                {/* 输入时间组件 */}
-                <Space direction="vertical" size={12}>
-                  <DatePicker
-                    onChange={(value, dateString) =>
-                      handleManyTime(value, dateString, set_student_begin_time1)
-                    }
-                  />
-                  <DatePicker
-                    onChange={(value, dateString) =>
-                      handleManyTime(value, dateString, set_student_end_time1)
-                    }
-                  />
-                </Space>
-              </div>
-            </div>
-            <div className="flex flex-col w-[33%] h-[100%] items-start">
-              <div className="flex w-[100%] h-[50%] items-center justify-center">
-                ID
-              </div>
-              <div className="flex w-[100%] h-[50%] items-center justify-center">
-                <Input onChange={handleID} />
-              </div>
-            </div>
-            <div className="flex flex-col w-[33%] h-[100%] items-start">
-              <div className="flex w-[100%] h-[50%] items-center justify-center">
-                管理员第一次匹配截止时间
-              </div>
-              <div className="flex w-[100%] h-[50%] items-center justify-center">
-                {" "}
-                {/*时间组件*/}
-                <Space direction="vertical">
-                  <DatePicker
-                    onChange={(value, dateString) =>
-                      handleManyTime(value, dateString, set_admin_end_time1)
-                    }
-                  />
-                </Space>
-              </div>
-            </div>
+          <div className="flex w-[50%] h-[100%] items-center justify-center">
+            专业：
+            <Input onChange={handleSetMajor} style={{ width: 300 }} />
           </div>
-          <div className="flex w-[100%] h-[30%] bg-pink-300">
-            <div className="flex flex-col w-[33%] h-[100%] items-start">
-              <div className="flex w-[100%] h-[50%] items-center justify-center">
-                学生第二次选题截止时间
-              </div>
-              <div className="flex w-[100%] h-[50%] items-center justify-center">
-                {" "}
-                {/*时间组件*/}
-                {/* 输入时间组件 */}
-                <Space direction="vertical" size={12}>
-                  <DatePicker
-                    onChange={(value, dateString) =>
-                      handleManyTime(value, dateString, set_student_end_time2)
-                    }
-                  />
-                </Space>
-              </div>
-            </div>
-            <div className="flex flex-col w-[33%] h-[100%] items-start">
-              <div className="flex w-[100%] h-[50%] items-center justify-center">
-                专业
-              </div>
-              <div className="flex w-[100%] h-[50%] items-center justify-center">
-                <Input onChange={handleSetMajor} />
-              </div>
-            </div>
-            <div className="flex flex-col w-[33%] h-[100%] items-start">
-              <div className="flex w-[100%] h-[50%] items-center justify-center">
-                管理员第二次匹配截止时间
-              </div>
-              <div className="flex w-[100%] h-[50%] items-center justify-center">
-                {" "}
-                {/*时间组件*/}
-                <Space direction="vertical">
-                  <DatePicker
-                    onChange={(value, dateString) =>
-                      handleManyTime(value, dateString, set_admin_end_time2)
-                    }
-                  />
-                </Space>
-              </div>
-            </div>
+        </div>
+        <div className="flex w-[100%] h-[20%] border border-gray-300">
+          <div className="flex w-[50%] h-[100%] items-center justify-center">
+            教师提交题目截止时间：
+            <Space direction="vertical">
+              <DatePicker
+                onChange={(value, dateString) =>
+                  handleManyTime(value, dateString, set_teacher_post_time)
+                }
+              />
+            </Space>
           </div>
-          <div className="flex w-[100%] h-[20%] items-center justify-center bg-pink-400">
-            <button
-              onClick={handleTime}
-              className="w-[20%] btn bg-blue-500 border-blue-500 hover:bg-blue-400 hover:border-blue-400 text-white rounded-full"
-            >
-              提交
-            </button>
+          <div className="flex w-[50%] h-[100%] items-center justify-center">
+            管理员审核题目截止时间：
+            <Space direction="vertical">
+              <DatePicker
+                onChange={(value, dateString) =>
+                  handleManyTime(value, dateString, set_admin_audit_time)
+                }
+              />
+            </Space>
           </div>
+        </div>
+        <div className="flex w-[100%] h-[20%] border border-gray-300">
+          <div className="flex w-[33%] h-[100%] items-center justify-center">
+            学生第一次选题开始时间：
+            <Space direction="vertical" size={12}>
+              <DatePicker
+                onChange={(value, dateString) =>
+                  handleManyTime(value, dateString, set_student_begin_time1)
+                }
+              />
+            </Space>
+          </div>
+          <div className="flex w-[33%] h-[100%] items-center justify-center">
+            学生第一次选题截止时间：
+            <Space direction="vertical" size={12}>
+              <DatePicker
+                onChange={(value, dateString) =>
+                  handleManyTime(value, dateString, set_student_end_time1)
+                }
+              />
+            </Space>
+          </div>
+          <div className="flex w-[33%] h-[100%] items-center justify-center">
+            管理员第一次匹配截止时间：
+            <Space direction="vertical">
+              <DatePicker
+                onChange={(value, dateString) =>
+                  handleManyTime(value, dateString, set_admin_end_time1)
+                }
+              />
+            </Space>
+          </div>
+        </div>
+        <div className="flex w-[100%] h-[20%] border border-gray-300">
+          <div className="flex w-[50%] h-[100%] items-center justify-center">
+            学生第二次选题截止时间：
+            <Space direction="vertical" size={12}>
+              <DatePicker
+                onChange={(value, dateString) =>
+                  handleManyTime(value, dateString, set_student_end_time2)
+                }
+              />
+            </Space>
+          </div>
+          <div className="flex w-[50%] h-[100%] items-center justify-center">
+            管理员第二次匹配截止时间：
+            <Space direction="vertical">
+              <DatePicker
+                onChange={(value, dateString) =>
+                  handleManyTime(value, dateString, set_admin_end_time2)
+                }
+              />
+            </Space>
+          </div>
+        </div>
+        <div className="flex w-[100%] h-[10%] items-center justify-center border border-gray-300">
+          <button
+            onClick={handleTime}
+            className="w-[20%] btn bg-blue-500 border-blue-500 hover:bg-blue-400 hover:border-blue-400 text-white rounded-full"
+          >
+            提交
+          </button>
         </div>
       </div>
     </div>
